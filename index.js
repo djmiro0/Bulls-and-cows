2c7c5c1c0794deaa44bccf2db5ba0ad44d19c0ac
Djordje <107116545+djmiro0@users.noreply.github.com>
2023-01-16 14:16:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 function bullsAndCows(secretNumber, guess) {
   let bulls = 0;
   let cows = 0;
-  let secretNumberArray = secretNumber.toString().split("");
-  let guessArray = guess.toString().split("");
-  for (let i = 0; i < 4; i++) {
-    if (typeof guess !== "number") {
-      return `Your guess "${guess}" is not a number. Please guess a number...`;
-    } else if (secretNumberArray[i] === guessArray[i]) {
-      bulls++;
-    } else if (secretNumberArray.includes(guessArray[i])) {
-      cows++;
+  let secretNumberArray = secretNumber.toString().split(""); //turn the number to string and into an array ['']
+  let guessArray = guess.toString().split(""); //same, so we can compare them
+  if (guessArray.length < 4) {
+    // check if the number has 4 digits
+    return `Please add ${4 - guessArray.length} number(s) to your guess`;
+  } else {
+    for (
+      let i = 0;
+      i < 4;
+      i++ //loop whoop
+    ) {
+      if (typeof guess !== "number") {
+        // check if the value is a number
+        return `Your guess "${guess}" is not a number. Please guess a number...`;
+      } else if (secretNumberArray[i] === guessArray[i]) {
+        //check if the number is on the same place as the secret number
+        bulls++; //add one to bulls
+      } else if (secretNumberArray.includes(guessArray[i])) {
+        //check if we have the guess number anywhere inside
+        cows++; // add one to cows
+      }
     }
   }
   return { bulls, cows };
 }
-
-console.log(bullsAndCows(1234, 1234));
-// const prompt = require("prompt-sync")({ sigint: true });
-// let name = prompt("What is your name? ");
-// console.log(`User's input is: ${name}`);
